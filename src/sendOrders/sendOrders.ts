632fc6a80670a894a12953a0f9954dff3578c190
Jiri Zoudun <jiri.zoudun@etnetera.cz>
2024-02-26 22:54:59 +0100

    send orders for 28.2.2024 (specific_date_run)

--- src/sendOrders/sendOrders.ts
+++ src/sendOrders/sendOrders.ts
@@ -28,7 +28,7 @@ const getCharities = async (): Promise<unknown> => {
 }
 
 const getDateAfter7days = (addSeconds = 0): Date => {
-  const date = new Date()
+  const date = new Date(2024, 1, 28);
   date.setDate(date.getDate() + 7)
   date.setUTCHours(0, 0, 0, 0)
   date.setUTCMinutes(date.getTimezoneOffset())
